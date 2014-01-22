e1006bf0dfcd59e5c3915226a4c79f60dc549151
Jaromir Tomek <jaromir.tomek@gmail.com>
2014-01-22 17:38:56 +0100

    Added source

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 UrlField
 ========
+
+Installation
+------------
+
+Just use the Composer:
+
+```
+$ cd path/to/your/atk14/project/
+$ php composer.phar require yarri/url-field
+```
+
+Optionaly you can link the field source file to the regular location
+
+```
+$ cd path/to/your/atk14/project/
+$ ln -s vendor/yarri/url-field/src/url_field.php app/fields/url_field.php
+```
+
+If you haven't yet the Composer installed, run the following commands
+```
+$ cd path/to/your/atk14/project/
+$ curl -sS https://getcomposer.org/installer | php
+```
---
+++ src/url_field.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Field accepts URL like
+ * 	* http://www.domain.com/
+ * 	* https://www.domain.com/
+ * 	* www.domain.com
+ */
+class UrlField extends RegexField{
+	function __construct($options = array()){
+		parent::__construct('/^(https?:\/\/|)[a-z0-9.-]+(|:[0-9]{1,6})(\/.*|)$/i',$options);
+		$this->update_messages(array(
+			"invalid" => _("This doesn't look like an URL"),
+		));
+	}
+
+	/**
+	 * This is RegexField`s hook method beeing called atomatically after a successful pattern matching.
+	 */
+	function processResult($value, $matches){
+		if($matches[1]==""){
+			$value = "http://$value";
+		}
+		if($matches[3]==""){
+			$value = "$value/";
+		}
+		return array(null, $value);
+	}
+}
