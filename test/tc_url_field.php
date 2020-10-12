c7649fdb61c0f02c909735f049cca5f3eac195af
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-10-12 12:21:19 +0200

    Basic auth username and password can be included into the give URL

--- test/tc_url_field.php
+++ test/tc_url_field.php
@@ -8,12 +8,20 @@ class TcUrlField extends TcBase {
 			"https://www.atk14.net" => "https://www.atk14.net/",
 			"www.atk14.net/about" => "http://www.atk14.net/about",
 			"www.atk14.net" => "http://www.atk14.net/",
+			
+			"https://username:password@www.atk14.net/" => "https://username:password@www.atk14.net/",
+			"username:password@www.atk14.net/" => "http://username:password@www.atk14.net/",
 		) as $url => $expected_url){
 			$cleaned_url = $this->assertValid($url);
 			$this->assertEquals($expected_url,$cleaned_url);
 		}
 
-		$err_msg = $this->assertInvalid("###");
-		$this->assertEquals("This doesn't look like an URL",$err_msg);
+		foreach(array(
+			"###",
+			"::@www.atk14.net",
+		) as $url){
+			$err_msg = $this->assertInvalid($url);
+			$this->assertEquals("This doesn't look like an URL",$err_msg);
+		}
 	}
 }
